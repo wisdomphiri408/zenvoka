3fd6f0acc54f29ec478afc307b74fb654af21908
Wisdom Phiri <wisdomphiri408@gmail.com>
2025-09-07 10:19:43 +0200

    Add categories and products pages

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const Categories: NextPage = () => {
+    return(
+        <div>
+            <h1 className="text-h1 text-center">
+                Categories Page
+            </h1>
+        </div>
+    )
+}
+
+export default Categories;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Products: NextPage = () => {
+    return(
+        <div>
+            <h1 className="text-h1 text-center">Product Listing Page</h1>
+        </div>
+    )
+}
+
+export default Products;
