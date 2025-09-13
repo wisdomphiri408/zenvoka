b9b9fbe605fddf1a75a1d5a4059287ee0d3ee34f
Wisdom Phiri <wisdomphiri408@gmail.com>
2025-09-13 16:44:58 +0200

    Fixing the card css layout in productcard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 interface Props {
     product:{
@@ -14,24 +15,30 @@ interface Props {
 
 const ProductCard: NextPage <Props>= ({product}) => {
     return(
-        <div key={product.id} className="p-4 border rounded">
-            <div>
-            <Image
-             src={product.image} 
-             alt={product.title}
-             width={600}
-             height={400} />
+        <div key={product.id} className="p-4 border rounded w-full max-w-[250px]">
+            <div className="card aspect-square relative rounded overflow-hidden">
+                <Image
+                src={product.image}
+                alt={product.title}
+                fill
+                style={{ objectFit: 'contain' }}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAH0lEQVR42mP8/5+hP6VQwMDA8J+FhwMDgYGjAwAIHhCqZ8b2swAAAABJRU5ErkJggg=="
+            />
+            </div>
+            <div className="flex">
+                <p className="text-sm bg-gray-100 rounded-full dark:bg-background-color-dark px-2">{product.category}</p>
             </div>
             <div>
-                <p>{product.category}</p>
+                <p className="text-body">{product.title}</p>
             </div>
             <div>
-                <p>{product.title}</p>
-            </div>
-            <div>
-                <p>${product.price}</p>
+                <p className="text-body font-bold">${product.price}</p>
                 <p>
-                    <button>Add</button>
+                    <Button
+                    variant={"primary"}
+                    className="p-2"
+                    >Add</Button>
                 </p>
             </div>
         </div>
