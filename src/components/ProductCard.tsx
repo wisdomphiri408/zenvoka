cdf6de17ac0636a9268fa81c5f41c7657cce9b37
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-09-13 18:28:11 +0200

    Enhance ProductCard layout with star rating and improve FeaturedTabs structure

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,47 +1,56 @@
 import { NextPage } from "next";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { Star, StarHalf } from "lucide-react";
+
 
 interface Props {
-    product:{
-        id:number;
-        image:string;
-        title:string;
-        price:number;
-        category:string;
-        description:string;
+    product: {
+        id: number;
+        image: string;
+        title: string;
+        price: number;
+        category: string;
+        description: string;
     }
 }
 
-const ProductCard: NextPage <Props>= ({product}) => {
-    return(
-        <div key={product.id} className="p-4 border rounded w-full max-w-[250px]">
+const ProductCard: NextPage<Props> = ({ product }) => {
+    return (
+        <div key={product.id} className="flex flex-col gap-2 p-4 border rounded w-full max-w-[250px]">
             <div className="card aspect-square relative rounded overflow-hidden">
                 <Image
-                src={product.image}
-                alt={product.title}
-                fill
-                style={{ objectFit: 'contain' }}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAH0lEQVR42mP8/5+hP6VQwMDA8J+FhwMDgYGjAwAIHhCqZ8b2swAAAABJRU5ErkJggg=="
-            />
+                    src={product.image}
+                    alt={product.title}
+                    fill
+                    style={{ objectFit: 'contain' }}
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAH0lEQVR42mP8/5+hP6VQwMDA8J+FhwMDgYGjAwAIHhCqZ8b2swAAAABJRU5ErkJggg=="
+                />
             </div>
-            <div className="flex">
+            <div className="flex items-center mt-2 mb-1 space-x-2">
                 <p className="text-sm bg-gray-100 rounded-full dark:bg-background-color-dark px-2">{product.category}</p>
+                <div className="flex items-center ml-auto">
+                    <Star className="w-4 h-4 fill-amber-400 dark:fill-amber-300" />
+                    <Star className="w-4 h-4 fill-amber-400 dark:fill-amber-300" />
+                    <Star className="w-4 h-4 fill-amber-400 dark:fill-amber-300" />
+                    <Star className="w-4 h-4 fill-amber-400 dark:fill-amber-300" />
+                    <StarHalf className="w-4 h-4 fill-amber-400 dark:fill-amber-300" />
+                </div>
             </div>
             <div>
                 <p className="text-body">{product.title}</p>
             </div>
-            <div>
+            <div className="flex items-center justify-between ">
                 <p className="text-body font-bold">${product.price}</p>
                 <p>
                     <Button
-                    variant={"primary"}
-                    className="p-2"
+                        variant={"primary"}
+                        className="p-2"
                     >Add</Button>
                 </p>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
--- src/components/home/FeaturedTabs.tsx
+++ src/components/home/FeaturedTabs.tsx
@@ -1,21 +1,21 @@
 'use client'
 import React, { useState } from "react";
-import { Funnel,ChevronDown, ChevronUp } from "lucide-react";
+import { Funnel, ChevronDown, ChevronUp } from "lucide-react";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import ProductCard from "../ProductCard";
 
 interface Product {
-    id:number;
-    title:string;
-    price:number;
-    image:string;
-    category:string;
-    description:string;
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+    category: string;
+    description: string;
 }
 
 interface TabData {
-    [key:string]:Product[];
+    [key: string]: Product[];
 }
 
 interface Props {
@@ -23,69 +23,71 @@ interface Props {
 }
 
 const tabs = [
-    {id: 'men',label: 'Men\'s clothing'},
-    {id: 'women',label: 'Women\'s clothing'},
-    {id: 'electronics',label: 'Electronics'},
-    {id: "jewelery",label: "Jewelery"},
+    { id: 'men', label: 'Men\'s clothing' },
+    { id: 'women', label: 'Women\'s clothing' },
+    { id: 'electronics', label: 'Electronics' },
+    { id: "jewelery", label: "Jewelery" },
 ]
 
-const FeaturedTabs: React.FC<Props> = ({tabData}) => {
+const FeaturedTabs: React.FC<Props> = ({ tabData }) => {
     const [activeTab, setActiveTab] = useState<string>('men');
     const [dropDown, setDropDown] = useState<boolean>(true);
 
 
-    return(
-        <div className="flex flex-col lg:flex-row lg:justify-center gap-2">
+    return (
+        <div className="flex flex-col lg:flex-row lg:justify-center gap-2 m-auto">
             {/* Tabs */}
-            <div className="card flex flex-col lg:w-[min(400px,20vw)]">
-            <div className="flex items-center justify-between mb-2">
-                <p className="flex gap-2 items-center">
-                    <Funnel className="w-4 h-4" />
-                    <span>Categories</span>   
-                </p> 
-                {dropDown 
-                ? <ChevronUp className="w-4 h-4" onClick={()=>setDropDown(false)}/>
-                : <ChevronDown className="w-4 h-4" onClick={() => setDropDown(true)}/>}      
-            </div>
-             
-             {dropDown && (
-                <div className="flex flex-col gap-1">
-                {tabs.map((tab) => (
-                    <Button
-                    key={tab.id}
-                    variant={activeTab === tab.id
-                        ? "primary"
-                        :"default"
-                    }
-                    onClick={() => setActiveTab(tab.id)}
-                    className={`w-full py-2 px-2 text-sm font-medium transition-colors duration-200 flex justify-between
+            <div>
+                <div className="card flex flex-col lg:w-[min(400px,20vw)]">
+                    <div className="flex items-center justify-between mb-2">
+                        <p className="flex gap-2 items-center">
+                            <Funnel className="w-4 h-4" />
+                            <span>Categories</span>
+                        </p>
+                        {dropDown
+                            ? <ChevronUp className="w-4 h-4" onClick={() => setDropDown(false)} />
+                            : <ChevronDown className="w-4 h-4" onClick={() => setDropDown(true)} />}
+                    </div>
+
+                    {dropDown && (
+                        <div className="flex flex-col gap-1">
+                            {tabs.map((tab) => (
+                                <Button
+                                    key={tab.id}
+                                    variant={activeTab === tab.id
+                                        ? "primary"
+                                        : "default"
+                                    }
+                                    onClick={() => setActiveTab(tab.id)}
+                                    className={`w-full py-2 px-2 text-sm font-medium transition-colors duration-200 flex justify-between
                         ${activeTab === tab.id ? "" : ""}
                     `}
-                    >
-                    {tab.label}
-                    {activeTab === tab.id && (
-                    <span className="m-2 p-1 rounded-md bg-background-color text-text-primary dark:bg-background-color-dark dark:text-text-secondary">Active</span>
+                                >
+                                    {tab.label}
+                                    {activeTab === tab.id && (
+                                        <span className="m-2 p-1 rounded-md bg-background-color text-text-primary dark:bg-background-color-dark dark:text-text-secondary">Active</span>
+                                    )}
+
+                                </Button>
+                            ))}
+                        </div>
                     )}
 
-                    </Button>
-                ))}
-                </div>
-             )}
-
-            </div>
-
-
-
-            {/* Tabs Content */}
-            <div>
-                <div className="grid grid-cols-1 gap-4">
-                {tabData[activeTab]?.map((product) => (
-                    <div key={product.id}>
-                     <ProductCard product={product}/>
-                    </div>
-                ))}
                 </div>
             </div>
+
+
+            {/* Product Grid */}
+            <div className="flex-1 m-auto sm:m-0">
+                <div className="grid gap-2 grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
+                    {tabData[activeTab]?.map((product) => (
+                        <div key={product.id}>
+                            <ProductCard product={product} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
 
         </div>
     )
