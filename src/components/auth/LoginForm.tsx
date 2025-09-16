769e5b24f65d68431637d2542050805ccf20297d
Wisdom Phiri <wisdomphiri408@gmail.com>
2025-09-16 10:15:53 +0200

    [200~feat(auth): Refactor and improve LoginForm component

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -4,38 +4,56 @@ import { Button } from "@/components/ui/button";
 import { NextPage } from "next";
 import InputField from "@/components/Inputfield";
 import { useState } from "react";
-import { ArrowLeft, User, Lock } from "lucide-react";
+import { ArrowLeft, User, Lock, EyeIcon,EyeClosed } from "lucide-react";
 
 const LoginForm: NextPage = () => {
     const [userName, setUserName] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const togglePassword = () => {
+        setShowPassword(prev => !prev)
+    }
 
 
     return (
-            <form className="flex flex-col gap-4 shadow-2xs inset-shadow-2xs max-w-[500px] m-auto p-4">
+            <form className="flex flex-col gap-4 border border-border-light dark:border-border-dark shadow-xl  m-auto p-4 rounded-2xl w-full max-w-[450px]">
                 <h5 className="text-h5">Sign In</h5>
                 <p className="text-small text-text-secondary">Enter your credentials to access your account</p>
 
                 <div className="flex flex-col gap-2 border-b-2 border-border-light pb-4 dark:border-border-dark">
                     <div className="flex flex-col items-start">
                     <label className="text-body">Username</label>
-                    <InputField
-                    type="text"
-                    className="w-full"
-                    value={userName}
-                    placeholder="Enter your username"
-                    onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUserName(e.target.value)}
-                    />
+                    <div className="w-full flex items-center">
+                        <User className="w-4 h-4 z-1" />
+                        <InputField
+                        type="text"
+                        className="w-full px-6 ml-[-20px]"
+                        value={userName}
+                        placeholder="Enter your username"
+                        onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUserName(e.target.value)}
+                        />
                     </div>
+                    </div>
+
                     <div className="flex flex-col items-start">
                     <label className="text-body">Password</label>
-                    <InputField
-                    type="password"
-                    placeholder="Enter your password"
-                    className="w-full"
-                    value={password}
-                    onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setPassword(e.target.value)}                    
-                    />
+                    <div className="w-full flex items-center">
+                        <Lock className="w-4 h-4 z-1"/>
+                        <InputField
+                        type= {showPassword?'text':'password'}
+                        placeholder="Enter your password"
+                        className="w-full px-6 ml-[-20px] mr-[-20px]"
+                        value={password}
+                        onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setPassword(e.target.value)}                    
+                        />
+                        {showPassword?(
+                            <EyeIcon className="w-4 h-4 cursor-pointer z-1" onClick={togglePassword}/>
+                        ):(
+                            <EyeClosed className="w-4 h-4 cursor-pointer z-1" onClick={togglePassword}/>
+                        )}
+                        
+                    </div>
                     </div>
                     <Button
                     variant={"primary"}
